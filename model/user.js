5c0c08401af20e1ad80ed66acb465eb40c64a434
Arun Kumar <exceltes@gmail.com>
2018-08-17 18:48:12 +0530

    registeration api

---
+++ model/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/express");
+
+
+let userSchema = new Schema({
+
+	  username:String,
+	  password:String,
+	  email:String,
+	  firstName:String,
+	  lastName:String
+});
+
+
+module.exports.User = mongoose.model("User",userSchema);
